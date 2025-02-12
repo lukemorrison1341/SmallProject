3616993ef8ac553b0b0dea482e92aedbdc61fac2
Donaldo Martinez <do073496@ucf.edu>
2025-02-12 12:17:56 -0500

    Create Test.php
    A test file to check the functionality of our PHP files.

---
+++ LAMPAPI/Test.php
@@ -0,0 +1,20 @@
+<?php
+    $inData = getRequestInfo();
+
+    $username = $inData["username"];
+	$password = $inData["password"];
+
+    $result = '{"username":"' . $username . '", password:"' . $password . '"}'
+    sendResultInfoAsJson($result)
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+?>
