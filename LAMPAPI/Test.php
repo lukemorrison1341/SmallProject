10a1c6803ace05dda41c40065e491c707559552f
Donaldo Martinez <do073496@ucf.edu>
2025-02-12 12:43:35 -0500

    Update Tests

--- LAMPAPI/Test.php
+++ LAMPAPI/Test.php
@@ -1,4 +1,10 @@
 <?php
     header('Content-type: application/json');
     sendResultInfoAsJson($_GET["username"]);
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
 ?>
--- LAMPAPI/Test2.php
+++ LAMPAPI/Test2.php
@@ -1,11 +1,16 @@
 <?php
     $inData = getRequestInfo();
 
-    header('Content-type: application/json');
-    echo $inData;
+    sendResultInfoAsJson($inData)
 
     function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
 	}
 ?>
