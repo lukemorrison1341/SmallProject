750339aaf6a3d2d55a28abc47c3aa094d0fb8773
Donaldo Martinez <do073496@ucf.edu>
2025-02-14 14:17:26 -0500

    Update api.js

--- api.js
+++ api.js
@@ -130,7 +130,11 @@ function searchContact()
 	
 	let colorList = "";
 
-	let tmp = {search:srch,userId:userId};
+	let tmp = {
+		search: srch,
+		userId: userId
+	};
+
 	let jsonPayload = JSON.stringify( tmp );
 
 	let url = urlBase + '/SearchContact.' + extension;
@@ -146,16 +150,7 @@ function searchContact()
 			{
 				let jsonObject = JSON.parse( xhr.responseText );
 				
-				for( let i=0; i<jsonObject.results.length; i++ )
-				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
-				}
-				console.log(colorList);
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
+				console.log(jsonObject);
 			}
 		};
 		xhr.send(jsonPayload);
