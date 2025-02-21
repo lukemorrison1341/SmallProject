3b9177bafe30e6a9c8feac09fdd026b0fa83c5af
Jesuel Rosado <jesuel.rosado01@protonmail.com>
2025-02-21 11:49:47 -0500

    modified DeleteContact.php to handle DELETE requests.

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,43 +1,30 @@
 <?php
 
-$inData = getRequestInfo();
-
-$userId = $inData["userId"];
-$firstName = $inData["firstName"];
-$lastName = $inData["lastName"];
+$userId = $_GET["userId"];
+$firstName = $_GET["firstName"];
+$lastName = $_GET["lastName"];
 
 $conn = new mysqli("localhost", "contact_app", "UniversalP@ssw0rd!", "contact_manager");
 
 if ($conn->connect_error) {
-
     returnWithError($conn->connect_error);
-
 } else {
-
     $stmt = $conn->prepare("DELETE FROM Contacts WHERE Name = ? AND UserID = ?");
     $name = $firstName . ' ' . $lastName;
     $stmt->bind_param("si", $name, $userId);
 
     if ($stmt->execute()) {
-
         if ($stmt->affected_rows > 0) {
-
             returnWithInfo("Contact Deleted.");
-
         } else {
-
             returnWithError("Contact Not Found.");
-
         }
+    } else {
+        returnWithError("Error executing delete query.");
     }
 
     $stmt->close();
     $conn->close();
-}
-
-function getRequestInfo()
-{
-    return json_decode(file_get_contents('php://input'), true);
 }
 
 function sendResultInfoAsJson($obj)
