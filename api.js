ec26cff06846fdb91fe6308c499786d876810802
Donaldo Martinez <do073496@ucf.edu>
2025-02-25 14:51:25 -0500

    Update api.js

--- api.js
+++ api.js
@@ -188,23 +188,33 @@ function addContact()
 	contact_div.classList.add("contact-info");
 	table_row.appendChild(contact_div);
 
+	let div = document.createElement('div');
+	div.classList.add("input-box");
 	let tmp = document.createElement('input');
+	document.createElement('input');
 	tmp.type = "text";
 	tmp.name = "name";
 	tmp.id = "name";
-	contact_div.appendChild(tmp);
+	div.appendChild(tmp);
+	contact_div.appendChild(div);
 
+	div = document.createElement('div');
+	div.classList.add("input-box");
 	tmp = document.createElement('input');
 	tmp.type = "email";
 	tmp.name = "email";
 	tmp.id = "email";
-	contact_div.appendChild(tmp);
+	div.appendChild(tmp);
+	contact_div.appendChild(div);
 
+	div = document.createElement("div");
+	div.classList.add("input-box");
 	tmp = document.createElement('input');
 	tmp.type = "tel";
 	tmp.name = "phone";
 	tmp.id = "phone";
-	contact_div.appendChild(tmp);
+	div.appendChild(tmp);
+	contact_div.appendChild(div);
 
 	tmp = document.createElement('div');
 	tmp.innerHTML = '<svg class="edit" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>';
