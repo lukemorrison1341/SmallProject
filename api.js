8cc712c4c73a21d8f3284dc2dcdb9e784ad6a84a
Donaldo Martinez <do073496@ucf.edu>
2025-02-25 14:46:20 -0500

    Update api.js

--- api.js
+++ api.js
@@ -181,7 +181,6 @@ function doLogout()
 
 function addContact()
 {
-	console.log("Add!");
 	const results_area = document.getElementById("results-box");
 	const table_row = document.createElement("div");
 	table_row.classList.add("contact-row");
@@ -213,6 +212,8 @@ function addContact()
 	tmp = document.createElement('div');
 	tmp.innerHTML = '<svg class="delete" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
 	table_row.appendChild(tmp);
+
+	results_area.prepend(table_row);
 }
 
 function searchContact()
