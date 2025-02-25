5bc5c19cee1ab3736d02be7c6eb4a0e1da0140a4
Donaldo Martinez <do073496@ucf.edu>
2025-02-25 09:20:13 -0500

    Update api.js

--- api.js
+++ api.js
@@ -300,8 +300,9 @@ function searchAllContacts()
 				}
 
 				for(let i = 0; i < jsonObject.contacts.length; i++) {
-					table_row = document.createElement('div');
+					const table_row = document.createElement('div');
 					table_row.classList.add("contact-row");
+					
 					tmp = document.createElement('p');
 					tmp.innerHTML = jsonObject.contacts[i].Name;
 					table_row.appendChild(tmp);
