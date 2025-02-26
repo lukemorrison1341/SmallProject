0c4392be71c4af443148156e27e632c178d1adf1
Donaldo Martinez <do073496@ucf.edu>
2025-02-25 20:21:03 -0500

    Update api.js

--- api.js
+++ api.js
@@ -378,10 +378,8 @@ function searchAllContacts()
 	
 }
 
-function deleteContact() {
-	let tmp = {
-
-	}
+function deleteContact(contact_id) {
+	console.log(contact_id);
 }
 
 function createContactElement(contactObject) {
@@ -409,7 +407,7 @@ function createContactElement(contactObject) {
 	tmp = document.createElement('div');
 	tmp.innerHTML = '<svg class="delete" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
 	tmp.addEventListener("click", function() {
-
+		deleteContact(contact_div.getAttribute("data-id"));
 	}, false);
 	
 	table_row.appendChild(tmp);
