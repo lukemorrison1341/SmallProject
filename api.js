0c0941639391bf348b7676ed52d54ca22ed1dd0b
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2025-02-26 20:44:44 -0500

    Update api.js

--- api.js
+++ api.js
@@ -478,6 +478,49 @@ function createContactElement(contactObject) {
 		let confirm = document.createElement('div');
 		confirm.innerHTML = '<svg id="confirm" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
 		table_row.appendChild(confirm);
+		confirm.addEventListener("click", function() {
+			if (!isValidEmail(email_field.value) && email_field !== "") {
+				return;
+			}
+			else if (!isValidPhoneNumber(phone_field.value) && phone_field.value !== "") {
+				return;
+			}
+
+			let req_body = {
+				id: contactObject.ID,
+				userId: userId,
+				Name: name_field.value,
+				Email: email_field.value,
+				Phone: (hasNoDashes(phone_field.value)) ? phone_field.value.replace(/(\d{3})(\d{3})(\d{4})(\d*)/, '$1-$2-$3') : phone_field.value
+			}
+
+			let jsonPayload = JSON.stringify( req_body );
+			let url = urlBase + '/UpdateContact.' + extension;
+			let xhr = new XMLHttpRequest();
+			xhr.open("PATCH", url, true);
+			xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+			try
+			{
+				xhr.onreadystatechange = function() 
+				{
+					if (this.readyState == 4 && this.status == 200) 
+					{
+						let jsonObject = JSON.parse( xhr.responseText );
+
+						if (jsonObject.error) {
+							return;
+						}
+
+						window.location.href = "contacts.html";
+					}
+				};
+				xhr.send(jsonPayload);
+			}
+			catch(err)
+			{
+				// Do nothing.
+			}
+		}, false);
 		cancel = document.createElement('div');
 		cancel.innerHTML = '<svg id="cancel" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
 		table_row.appendChild(cancel);
