4c9c30efe06827b4b77914a469ed5b4c6881c88d
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2025-02-26 20:59:01 -0500

    Update api.js

--- api.js
+++ api.js
@@ -535,6 +535,13 @@ function createContactElement(contactObject) {
 
 // Show password toggle
 document.addEventListener('DOMContentLoaded', function() {
+	const logout_btn = document.querySelector("#logout-div svg");
+	if (logout_btn) {
+		logout_btn.addEventListener("click", function() {
+			doLogout();
+		});
+	}
+	
     const showPasswordToggle = document.getElementById("show-password-toggle");
     const hidePasswordToggle = document.getElementById("hide-password-toggle");
     const passwordField = document.getElementById("password");
@@ -555,14 +562,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
         });
-    
-	console.log("Error");
-	const logout_btn = document.querySelector("#logout-div svg");
-	if (logout_btn) {
-		logout_btn.addEventListener("click", function() {
-			doLogout();
-		});
-	}
 
 function isValidEmail(email) {
 	const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
