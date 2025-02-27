d1ee767a9f1efd164a0b5a992634f94b984fb4f4
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2025-02-26 20:02:33 -0500

    Update api.js

--- api.js
+++ api.js
@@ -180,7 +180,7 @@ function doLogout()
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	document.cookie = "userId= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
 	window.location.href = "index.html";
 }
 
